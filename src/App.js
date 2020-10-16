0e89a82e42f92f272570f0efe30265e3c098ad00
yoshida874 <ud8mnzdd@gmail.com>
2020-10-16 16:09:45 +0900

    RouteをHeader.jsに移動

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from "react";
-import Home from "./Component/Home/Home";
-import About from "./Component/About/About";
 import Header from "./Component/Header/Header";
-import Works from "./Component/Works/Works";
-import Contact from "./Component/Contact/Contact";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const App = () => {
   return (
     <Router>
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/About" component={About} />
-          <Route path="/Works" component={Works} />
-          <Route path="/Contact" component={Contact} />
-        </Switch>
-      </div>
+      <Header />
     </Router>
   );
 };
