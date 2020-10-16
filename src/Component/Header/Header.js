15fd652544bc6b14f0d288ec61284c58224c4bdc
yoshida874 <ud8mnzdd@gmail.com>
2020-10-16 16:06:32 +0900

    linkをbuttonのcomponentへ

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,10 +1,21 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { Route, Switch, Link } from "react-router-dom";
+
+import Home from "../Home/Home";
+import About from "../About/About";
+import Works from "../Works/Works";
+import Contact from "../Contact/Contact";
+
+import { makeStyles,createMuiTheme } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import { Link } from "react-router-dom";
+import ContactsIcon from "@material-ui/icons/Contacts";
+import WorkIcon from "@material-ui/icons/Work";
+import HomeIcon from "@material-ui/icons/Home";
+import PermIdentityIcon from "@material-ui/icons/PermIdentity";
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,28 +36,32 @@ export default function ButtonAppBar() {
 
   return (
     <div className={classes.root}>
-      <AppBar
-        position="static"
-        style={{ color: "#e0f2f1", backgroundColor: "#212121" }}
-      >
+      <AppBar style={{ color: "#e0f2f1", backgroundColor: "#212121" }}>
         <Toolbar>
           <Typography variant="h4" className={classes.title}>
             Vitamin Profile
           </Typography>
-          <Link to="/">
-            <Button className={classes.Button}>Home</Button>
-          </Link>
-          <Link to="/about">
-            <Button className={classes.Button}>about</Button>
-          </Link>
-          <Link to="/works">
-            <Button className={classes.Button}>works</Button>
-          </Link>
-          <Link to="/contact">
-            <Button className={classes.Button}>cotact</Button>
-          </Link>
+            <Button className={classes.Button} component={Link} to="/" startIcon={<HomeIcon />}>
+              Home
+            </Button>
+            <Button className={classes.Button} component={Link} to="/about" startIcon={<PermIdentityIcon />}>
+              about
+            </Button>
+            <Button className={classes.Button} component={Link} to="/works" startIcon={<WorkIcon />}>
+              works
+            </Button>
+            <Button className={classes.Button} component={Link} to="/contact" startIcon={<ContactsIcon />}>
+              cotact
+            </Button>
+
         </Toolbar>
       </AppBar>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/About" component={About} />
+        <Route path="/Works" component={Works} />
+        <Route path="/Contact" component={Contact} />
+      </Switch>
     </div>
   );
 }
