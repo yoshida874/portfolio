c3db3356fd2e17afdf34f26ca7d526faf1acfb9b
yoshida874 <ud8mnzdd@gmail.com>
2020-10-23 15:26:39 +0900

    フォントの変更

---
+++ src/Theme.js
@@ -0,0 +1,11 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      '"M PLUS 1p"'
+    ].join(','),
+  },
+});
+
+export default theme;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import theme  from "./Theme.js"
+import {MuiThemeProvider} from '@material-ui/core/styles';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MuiThemeProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </MuiThemeProvider>,
   document.getElementById("root")
 );
 
